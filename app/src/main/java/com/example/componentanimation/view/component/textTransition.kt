48f813f3baf34af2eb4636aeba55adeaf6842e1f
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-15 17:11:37 +0530

    jetpack animation

--- app/src/main/java/com/example/componentanimation/view/component/textTransition.kt
+++ app/src/main/java/com/example/componentanimation/view/component/textTransition.kt
@@ -1,18 +1,15 @@
 package com.example.componentanimation.view.component
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Snackbar
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -23,6 +20,10 @@ fun textTransition() {
         modifier = Modifier.fillMaxSize()
     ) {
         SnackBarDemo()
+        Spacer(modifier = Modifier.padding(10.dp))
+        SwitchDemo()
+        Spacer(modifier = Modifier.padding(10.dp))
+        AlertDialogSample()
     }
 }
 @Composable
@@ -48,5 +49,67 @@ fun SnackBarDemo() {
                 modifier = Modifier.padding(8.dp)
             ) { Text(text = "This is a snackbar!") }
         }
+    }
+}
+
+@Composable
+fun SwitchDemo() {
+    val checkedState = remember { mutableStateOf(true) }
+    Switch(
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it }
+    )
+}
+
+@Composable
+fun AlertDialogSample() {
+    MaterialTheme {
+        Column {
+            val openDialog = remember { mutableStateOf(false)  }
+
+            Button(onClick = {
+                openDialog.value = true
+            }) {
+                Text("Click me")
+            }
+
+            if (openDialog.value) {
+
+                AlertDialog(
+                    modifier = Modifier.clip(RoundedCornerShape(20.dp))
+                    ,onDismissRequest = {
+                        // Dismiss the dialog when the user clicks outside the dialog or on the back
+                        // button. If you want to disable that functionality, simply use an empty
+                        // onCloseRequest.
+                        openDialog.value = false
+                    },
+                    title = {
+                        Text(text = "Dialog Title")
+                    },
+                    text = {
+                        Text("Here is a text ")
+                    },
+                    confirmButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the Confirm Button")
+                        }
+                    },
+                    dismissButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the dismiss Button")
+                        }
+                    }
+                )
+            }
+        }
+
     }
 }
